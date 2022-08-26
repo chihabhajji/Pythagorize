fe8e57798dc30ec04e0486584c6a36184c3fef34
chihabhajji <blackpang23@gmail.com>
2022-08-26 15:02:22 +0100

    Functional

--- index.js
+++ index.js
@@ -1,14 +1,10 @@
+const PythegorizeAscii = (n) => n < 106 ? n - 96 :  PythegorizeAscii(n - 9);
 var num = Array.from("chihab ben hichem ben mohamed hajji")
 .filter((c) => c != " ")
-.map((c) => c.toLowerCase().charCodeAt(0));
+.map((c) => PythegorizeAscii(c.toLowerCase().charCodeAt(0)))
+.reduce((accumulator, curr) => accumulator + curr);
 
-var sum = 0;
-for (var i = 0; i < num.length; i++) {
-    sum += PythegorizeAscii(num[i]);
-}
+console.log(num);
 
-console.log(" FINAL : " + sum);
 
-function PythegorizeAscii(n){
-    return n < 106 ? n - 96 :  PythegorizeAscii(n - 9);
-}
+
