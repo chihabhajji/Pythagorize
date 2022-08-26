0dc5c4bf900b14dd18cce4057ef3c59cb3492531
chihabhajji <blackpang23@gmail.com>
2022-08-26 15:14:31 +0100

    Dig Sum added

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const PythegorizeAscii = (n) => n < 106 ? n - 96 :  PythegorizeAscii(n - 9);
+const digSum = (n) => n == 0 ? 0 : (n % 9 == 0) ? 9 : (n % 9);
 var num = Array.from("chihab ben hichem ben mohamed hajji")
 .filter((c) => c != " ")
 .map((c) => PythegorizeAscii(c.toLowerCase().charCodeAt(0)))
 .reduce((accumulator, curr) => accumulator + curr);
 
-console.log(num);
+console.log(digSum(num));
 
 
 
